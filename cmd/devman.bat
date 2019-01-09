cf4d46edb4c7ee5d7546cf8565d2b8bc492f6822
keithpenney <31521048+keithpenney@users.noreply.github.com>
2019-01-09 10:27:34 -0800

    Handy nonpresent devices envar

---
+++ cmd/devman.bat
@@ -0,0 +1,5 @@
+:: Start Windows device manager with nonpresent devices shown
+:: NOTE! MUST RUN AS ADMINISTRATOR
+:: Then make sure to select "Show Hidden Devices" from the view menu
+set devmgr_show_nonpresent_devices=1
+start devmgmt.msc
